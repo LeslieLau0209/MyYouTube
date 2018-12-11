21f67370e71c41ebc60ff296d640147609056267
Hao Liu <haol4@illinois.edu>
2018-12-11 16:43:09 +0800

    Last one

--- app/src/main/java/com/example/haoliu/myyoutube/MainActivity.java
+++ app/src/main/java/com/example/haoliu/myyoutube/MainActivity.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends YouTubeBaseActivity {
-
+    // Explain.
     private static final String TAG = "MainActivity";
 
 
