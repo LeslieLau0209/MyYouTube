63aabb3fba60c5a717800f75e3b979a220c8ad5e
Hao Liu <haol4@illinois.edu>
2018-12-11 16:41:51 +0800

    final

---
+++ app/src/main/java/com/example/haoliu/myyoutube/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.haoliu.myyoutube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
+
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    private static final String TAG = "MainActivity";
+
+
+    YouTubePlayerView mYouTubePlayerView;
+    Button ButtonPlay;
+    YouTubePlayer.OnInitializedListener mOnInitializedListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate:Starting.");
+        ButtonPlay = (Button) findViewById(R.id.ButtonPlay);
+        mYouTubePlayerView = (YouTubePlayerView) findViewById(R.id.YouTubePlayer);
+
+        mOnInitializedListener = new OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d(TAG, "onInitializationSuccess: Done Initializing");
+//                List<String> videoList = new ArrayList<>();
+//                videoList.add("");
+//                videoList.add("");
+//                youTubePlayer.loadVideo("");
+                youTubePlayer.loadPlaylist("PL4C2OaC1jQqR3ICDBf4j1dH1Fk4uIo-Lx");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d(TAG, "onInitializationSuccess: Fail to Initialize");
+
+            }
+        };
+        ButtonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Initializing YouTube Player.");
+                mYouTubePlayerView.initialize(YouTubeConfig.getApiKey(), mOnInitializedListener);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/haoliu/myyoutube/YouTubeConfig.java
@@ -0,0 +1,12 @@
+package com.example.haoliu.myyoutube;
+
+public class YouTubeConfig {
+
+    public YouTubeConfig() {
+    }
+    private static final String API_KEY = "AIzaSyB9jsnhZxTk3P3AgAPMR01yvazXgmy_IqU";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
